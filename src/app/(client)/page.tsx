8b39ef93508221f22262096b37779a30ff030593
Eury Sosa Garcia <112292926+watercubz@users.noreply.github.com>
2024-04-18 13:40:59 -0400

    feat: ⚙ footer implementation

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,6 +1,12 @@
+import Footer from "@/components/footer/page";
 import Navbar from "@/components/navbar/page";
 export default function Home() {
   return (
-    <Navbar/>
+    <>
+       <Navbar />
+      <div className="border-b border-slate-800"> {/* Línea divisora */}
+      </div>
+      <Footer />
+    </>
   );
 }
--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -3,13 +3,12 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Disclosure } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { useEffect, useState } from "react";
-import Image from "next/image";
 
 const navigation = [
-  { name: 'Home', href: '#', current: true },
+  { name: 'Home', href: '', current: true },
   { name: 'Cursos', href: '#', current: false },
   { name: 'Creadores', href: '#', current: false },
-  { name: 'About', href: '#', current: false },
+  { name: 'About', href: '/', current: false },
 ]
 
 function classNames(...classes: string[]) {
@@ -49,74 +48,79 @@ export default function Navbar() {
 
   return (
     <Disclosure as="nav" className="bg-gray-900">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <span className="font-bold">Freedev<a className="text-yellow-500">Tube</a></span>
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button className="w-40 h-10 gap-0 focus:outline-none text-black bg-yellow-500 rounded-lg py-2 transition duration-150 ease-in" onClick={handleAuthAction}>
-                  {isLoggedIn ? 'Cerrar sesión' : 'Iniciar sesión'}
-                </button>
-                {isLoggedIn && (
-                  <Image width={80} height={40} src="/profile.png" alt="Perfil de usuario" className="w-10 h-10 gap-6  rounded-full m-4" />
-                
+    {({ open }) => (
+      <>
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <span className="absolute -inset-0.5" />
+                <span className="sr-only">Open main menu</span>
+                {open ? (
+                  <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                 )}
+              </Disclosure.Button>
+            </div>
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex flex-shrink-0 items-center">
+                <span className="font-bold">Freedev<a className="text-yellow-500">Tube</a></span>
               </div>
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex space-x-4">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'rounded-md px-3 py-2 text-sm font-medium'
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <button className="w-40 h-10 gap-0 focus:outline-none text-black bg-yellow-500 rounded-lg py-2 transition duration-150 ease-in" onClick={handleAuthAction}>
+                {isLoggedIn ? 'Cerrar sesión' : 'Iniciar sesión'}
+              </button>
+              {/* {isLoggedIn && (
+                <Image width={80} height={40} src="/profile.png" alt="Perfil de usuario" className="w-10 h-10 gap-6  rounded-full m-4" />
+              )} */}
             </div>
           </div>
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+        </div>
+        <Disclosure.Panel className="sm:hidden">
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            {navigation.map((item) => (
+              <Disclosure.Button
+                key={item.name}
+                as="a"
+                href={item.href}
+                className={classNames(
+                  item.href  ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'block rounded-md px-3 py-2 text-base font-medium'
+                )}
+                aria-current={item.current ? 'page' : undefined}
+              >
+                {item.name}
+              </Disclosure.Button>
+            ))}
+            <button className="w-full mt-4 focus:outline-none text-black bg-yellow-500 rounded-lg py-2 transition duration-150 ease-in" onClick={handleAuthAction}>
+              {isLoggedIn ? 'Cerrar sesión' : 'Iniciar sesión'}
+            </button>
+            {/* {isLoggedIn && (
+              <Image width={80} height={40} src="/profile.png" alt="Perfil de usuario" className="w-10 h-10 gap-6  rounded-full m-4" />
+            )} */}
+          </div>
+        </Disclosure.Panel>
+      </>
+    )}
+  </Disclosure>
   )
 }
