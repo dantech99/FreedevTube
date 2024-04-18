b13f38e1724ad52173bdbe4107b1d1520dfd3f83
Eury Sosa Garcia <112292926+watercubz@users.noreply.github.com>
2024-04-18 14:14:06 -0400

    fix: footer error

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -3,9 +3,6 @@
 export default function Footer() {
   return (
     <>
-      <div style={{ minHeight: 'calc(100vh - 64px)', paddingBottom: '64px' }}>
-            <h2>UN ELEMENTO IRA AQUI</h2>
-      </div>
       <footer className="footer px-10 py-4 border-t bg-gray-900   fixed bottom-0 w-full">
         <aside className="items-center grid-flow-col">
           <p className="text-white font-bold">
