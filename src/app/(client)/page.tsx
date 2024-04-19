0654da897cba7179fc7324182fdd260421556a67
Eury Sosa Garcia <112292926+watercubz@users.noreply.github.com>
2024-04-19 11:59:18 -0400

    feat: resource card section implementation

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,11 +1,13 @@
 import DiscoverPage from '@/components/discover';
 import JumbotronSection from '@/components/jumbrotron';
+import ResourceCard from '@/components/cardSources';
 export default function Home() {
   return (
     <>
       {/* jumbrotron */}
       <JumbotronSection />
       <DiscoverPage />
-    </>
+       <ResourceCard />
+      </>
   );
 }
