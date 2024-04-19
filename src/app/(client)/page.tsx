1922c60a87ae8a9066bb2bbe19bad3d1cea5b47b
Danilo Antonio <69212530+dantech99@users.noreply.github.com>
2024-04-19 11:05:29 -0500

    Revert "feat: resource card section implementation"

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,14 +1,11 @@
 import DiscoverPage from '@/components/home/discover';
 import JumbotronSection from '@/components/home/jumbrotron';
-import ResourceCard from '@/components/cardSources';
-
 export default function Home() {
   return (
     <>
       {/* jumbrotron */}
       <JumbotronSection />
       <DiscoverPage />
-       <ResourceCard />
-      </>
+    </>
   );
 }
