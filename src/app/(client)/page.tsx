2ebd3d676d4df150939e6e14779832f49f9565c5
Eury Sosa Garcia <112292926+watercubz@users.noreply.github.com>
2024-04-20 12:03:35 -0400

    fix: error button server

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,18 +1,16 @@
 import DiscoverPage from '@/components/home/discover';
 import JumbotronSection from '@/components/home/jumbrotron';
 import { AuthButtonServer } from '@/components/home/auth-button-server';
-
 import ResourceCard from '@/components/home/cardSources';
 
 export default function Home() {
   return (
     <> 
-      <AuthButtonServer/>
       {/* jumbrotron */}
-     
       <JumbotronSection />
       <DiscoverPage />
       <ResourceCard />
+      <AuthButtonServer />
       </>
   );
 }
--- src/components/home/auth-button-server.tsx
+++ src/components/home/auth-button-server.tsx
@@ -1,17 +1,17 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation';
-import { AuthButton } from './auth-button-client';
 
 export async function AuthButtonServer () {
   const supabase = createServerComponentClient({ cookies })
   const { data: { user } } = await supabase.auth.getUser()
 
-  if(user === null) {
+  if(!user) {
     redirect('/unauthorized')
   }  
 
    return (
-    <></>
+    <>
+    </>
    )
 }
