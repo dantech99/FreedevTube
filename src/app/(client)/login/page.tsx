52398b3b31680f8c0949ec031dc3d1a7a47c448b
DanTech99 <antoniomcs21@gotmail.com>
2024-04-21 16:25:27 -0500

    Refactor import paths in login/page.tsx and page.tsx

--- src/app/(client)/login/page.tsx
+++ src/app/(client)/login/page.tsx
@@ -1,4 +1,4 @@
-import { AuthButtonServer } from '@/app/components/auth-button-server';
+import { AuthButtonServer } from "@/components/navbar/auth-button-server";
 
 export default function Login() {
   return (
