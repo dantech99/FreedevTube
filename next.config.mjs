4a0403d7805a8f82301dc71613270c81be7e1614
DanTech99 <antoniomcs21@gotmail.com>
2024-04-21 18:08:37 -0500

    Refactor next.config.mjs to remove unused image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-  images: {
-    remotePattens: [
-      {
-        protocol: 'https',
-        hostname: 'yt3.googleusercontent.com',
-        port: '',
-        pathname: '/account123/**',
-      },
-    ],
-  },
-};
-
 export default nextConfig;
