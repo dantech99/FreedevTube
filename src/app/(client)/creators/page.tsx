6860c8b3d2e19dd1f7253c9f601464eb08a338cc
AxolotlJ-Dev <kalelcg.eg@gmail.com>
2024-04-21 12:09:58 -0600

    page creators

---
+++ src/app/(client)/creators/page.tsx
@@ -0,0 +1,11 @@
+import Card from "@/components/creators/Card";
+import Header from "@/components/creators/Header";
+
+export default function Page() {
+    return(
+        <>
+            <Header />
+            <Card />
+        </>
+    )
+}
