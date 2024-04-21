da0dc23ac36d962e20283c168a5c8cfb0809e754
DanTech99 <antoniomcs21@gotmail.com>
2024-04-21 18:12:47 -0500

    Refactor Card component to use a grid layout and update button styles

--- src/components/creators/Card.tsx
+++ src/components/creators/Card.tsx
@@ -11,33 +11,31 @@ export default async function Card() {
 
   const {data: creadores} = await supabase.from('creadores').select('*')
   return (
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6 text-lg md:text-xl lg:text-2xl xl:text-2xl ">
-        {creadores?.map((creador) => (
-          <div
-            className="max-w-[404px] w-auto bg-[#13203E] flex flex-col justify-center items-center mx-auto p-4 "
-            key={creador.id}>
-            <img
-              className="rounded-full aspect-square h-[226px] mb-6"
-              src={creador.url_imagen}
-              alt={creador.nombre}
-              
-            />
-            <p className="font-extrabold text-yellow-500 text-xxl mb-6">{creador.nombre}</p>
-            <p className=" text-sm px-4 mb-6">{creador.descripcion}</p>
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6 text-lg md:text-xl lg:text-2xl xl:text-2xl">
+{creadores?.map((creador) => (
+  <div
+    className="max-w-[404px] bg-[#13203E] flex flex-col justify-center items-center mx-auto p-4 border-b-gray-800 rounded-lg "
+    key={creador.id}
+  >
+    <img
+      className="rounded-full aspect-square h-[226px] mb-6"
+      src={creador.url_imagen}
+      alt={creador.nombre}
+    />
+    <p className="font-extrabold text-yellow-500 text-xxl mb-6">{creador.nombre}</p>
+    <p className=" text-sm px-4 mb-6">{creador.descripcion}</p>
 
-            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 p-4 w-full">
-
-              <a href={creador.url_apoyar} className="bg-yellow-500 hover:bg-yellow-400 text-black text-md rounded-full px-2 m-1 py-1 text-center" target="_blank">
-                Apoyar
-              </a>
-              <button className="bg-yellow-500 hover:bg-yellow-400 text-black text-md rounded-full px-2 m-1 py-1 text-center">
-
-                Saber Más
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
+    <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 p-4 w-full">
+      <a href={creador.url_apoyar} className="inline-block px-3 py-2 text-sm font-medium text-center text-black bg-yellow-400 rounded-lg hover:bg-yellow-700 focus:ring-4 focus:outline-none focus:ring-blue-300 mr-2">
+        Apoyar
+      </a>
+      <button className="inline-block px-3 py-2 text-sm font-medium text-center text-black bg-yellow-400 rounded-lg hover:bg-yellow-700 focus:ring-4 focus:outline-none focus:ring-blue-300">
+        Saber Más
+      </button>
+    </div>
+  </div>
+))}
+</div>
 
   );
 }
