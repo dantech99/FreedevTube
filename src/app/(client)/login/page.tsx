905170fd09ca6c2f7345f12fd448a00dd21ef500
Eury Sosa Garcia <112292926+watercubz@users.noreply.github.com>
2024-04-24 14:40:07 -0400

    Resolved error login

--- src/app/(client)/login/page.tsx
+++ src/app/(client)/login/page.tsx
@@ -1,14 +1,15 @@
+import { AuthButtonServer } from "@/components/navbar/auth-button-server";
 import { ButtonServer } from "@/components/navbar/button-server";
 
 export default function Login() {
   return (
     <section className="flex h-screen justify-center items-center flex-col gap-5">
-      {/* <div className="w-96 h-96 flex justify-center items-center border flex-col p-4 gap-10">
+      <div className="w-96 h-96 flex justify-center items-center border flex-col p-4 gap-10">
         <h1 className="text-2xl text-balance text-center">
           Para acceder al contenido inicia session primero
         </h1>
-        <ButtonServer />
-      </div> */}
+        <AuthButtonServer />
+      </div>
     </section>
   );
 }
