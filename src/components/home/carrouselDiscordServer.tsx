45795af38e9bb09ce439ecb18712b7decd3779e8
DanTech99 <antoniomcs21@gotmail.com>
2024-04-25 13:04:55 -0500

    Add CarrouselDiscordServer component to home page

---
+++ src/components/home/carrouselDiscordServer.tsx
@@ -0,0 +1,13 @@
+export default function CarrouselDiscordServer() {
+
+  // hacer un llamado a la api de discord para traer los servidores que quiero traerme de acuerdo a un listado de ids de  servidores
+
+  
+  
+
+  return (
+    <section className="carrousel">
+      aqui ira el carrousel
+    </section>
+  )
+}
