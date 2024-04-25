6bc629d3f69666e0980f4bc9716f8f57144061c4
DanTech99 <antoniomcs21@gotmail.com>
2024-04-24 21:41:58 -0500

    ➕ Add new system off auth, remplace supabase auth for auth,js, implement auth with github

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -3,8 +3,8 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/navbar/navbar';
 import Footer from '@/components/footer/footer';
-import {NextUIProvider} from "@nextui-org/react";
-
+import { auth } from '@/auth';
+import { SessionProvider } from 'next-auth/react';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -18,28 +18,39 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
+  if (session?.user) {
+    // TODO: Look into https://react.dev/reference/react/experimental_taintObjectReference
+    // filter out sensitive data before passing to client.
+    session.user = {
+      name: session.user.name,
+      email: session.user.email,
+      image: session.user.image,
+    }
+  }
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='bg-gradient '>
-        {/* Layout UI */}
+        <div className="bg-gradient ">
+          {/* Layout UI */}
 
-        {/* navbar  */}
-        <div className='border-b border-slate-950'>
+          {/* navbar  */}
+          <div className="border-b border-slate-950">
+            <SessionProvider  session={session}>
+              <Navbar />
+            </SessionProvider>
+          </div>
+          {/* main content  */}
 
-        <Navbar />
+          <main className="max-w-screen-xl px-4 py-8 mx-auto lg:py-5 ">
+            {children}
+          </main>
+
+          {/* footer  */}
+          <Footer />
         </div>
-        {/* main content  */}
-     
-            <main className='max-w-screen-xl px-4 py-8 mx-auto lg:py-5 '>
-              {children}
-            </main>
-        
-        {/* footer  */}
-        <Footer />
-        </div>
-
-
       </body>
     </html>
   );
