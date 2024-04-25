882d87db851da494185ac6c0828bb3773972f1f8
DanTech99 <antoniomcs21@gotmail.com>
2024-04-24 21:49:18 -0500

    fix: componentes no imported in page app 🔧

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,7 +1,6 @@
 import DiscoverPage from '@/components/home/discover';
 import JumbotronSection from '@/components/home/jumbrotron';
 
-import { AuthButtonServer } from '@/components/navbar/auth-button-server';
 
 import ResourceCard from '@/components/home/cardSources';
 import Tecnology from '@/components/home/tecnology';
