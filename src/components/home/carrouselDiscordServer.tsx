e41b914c219178621cf06757d78c4189bcab0df7
DanTech99 <antoniomcs21@gotmail.com>
2024-04-25 19:42:08 -0500

    Refactor CarrouselDiscordServer component and update styles

--- src/components/home/carrouselDiscordServer.tsx
+++ src/components/home/carrouselDiscordServer.tsx
@@ -1,13 +1,34 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 export default function CarrouselDiscordServer() {
+  const serverIds = ['id1', 'id2', 'id3']; // reemplace con sus propios IDs de servidor
+  const [servers, setServers] = useState([]);
 
-  // hacer un llamado a la api de discord para traer los servidores que quiero traerme de acuerdo a un listado de ids de  servidores
+  useEffect(() => {
+    const fetchServers = async () => {
+      const serversData = [];
+      for (const id of serverIds) {
+        const response = await axios.get(`https://discord.com/api/guilds/${id}`, {
+          headers: {
+            'Authorization': `Bot ${process.env.DISCORD_BOT_TOKEN}`
+          }
+        });
+        serversData.push(response.data);
+      }
+      setServers(serversData);
+    };
 
-  
-  
+    fetchServers();
+  }, []);
 
   return (
     <section className="carrousel">
-      aqui ira el carrousel
+      {servers.map((server, index) => (
+        <div key={index}>
+          {server.name} // reemplace con los datos que desea mostrar
+        </div>
+      ))}
     </section>
-  )
+  );
 }
