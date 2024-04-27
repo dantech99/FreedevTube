1b2b1177937f3712a5f4ce67a3f3813ad6e02d50
DanTech99 <antoniomcs21@gotmail.com>
2024-04-27 18:58:09 -0500

    Fix link in HeaderImage component

--- src/components/headers/HeaderImage.tsx
+++ src/components/headers/HeaderImage.tsx
@@ -52,7 +52,7 @@ export default async function HeaderImage({
                 Apoyar
               </a>
               <Link
-                href={`http://localhost:3000/creators/${creadores.slug}`}
+                href={`/creators/${creadores.slug}`}
                 className="inline-block px-3 py-2 text-sm font-medium text-center text-black bg-yellow-500 rounded-lg hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-blue-300 mb-3"
               >
                 Saber Más
